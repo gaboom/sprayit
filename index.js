35005920f4f5059ba2697171b9a242fd7bc5d0cf
gab <gab@gablog.eu>
2014-10-29 00:40:07 +0800

    Fix responsive loading screen. Introduce canvas.

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 var app = angular.module("sprayit", []);
 
 app.controller("SprayController", function($scope, $timeout) {
-  var reset = null;
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  function init() {
+    $content = $("#content");
+    context.canvas.width = $content.width();
+    context.canvas.height = $content.height();
+    context.strokeStyle = 'blue';
+    context.lineWidth = '5';
+    context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
+  }
 
+  var reset = null;
   $scope.reset = function() {
     if (reset) {
       clearTimeout(reset);
     } else {
       $timeout(function(){
-        $scope.loading = true;
+        $scope.loaded = false;
       });
     }
     reset = setTimeout(function() {
       reset = null;
+      init();
       $timeout(function(){
-        $scope.loading = false;
+        $scope.loaded = true;
       });
     }, 500);
   };
-
   $(window).resize($scope.reset);
+
+  init();
+  $scope.loaded = true;
 });
 
