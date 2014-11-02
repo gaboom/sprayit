40108dfcdb29d8c265d496b778829147d8a27743
gab <gab@gablog.eu>
2014-11-02 19:15:03 +0800

    Spraying enabled :)))

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = angular.module("sprayit", []);
 
 app.controller("SprayController", function($scope, $timeout) {
-  $timeout(function(){
+  $timeout(function() {
     $scope.reset();
   });
 });
@@ -41,13 +41,35 @@ app.directive("sprayCanvas", function($timeout) {
     restrict: 'A',
     link: function($scope, element) {
       var context = element[0].getContext("2d");
+      
+      var isDrawing = false;
+      context.canvas.onmousedown = function(e) {
+        isDrawing = true;
+        context.lineWidth = 10;
+        context.lineJoin = context.lineCap = 'round';
+        context.shadowBlur = 10;
+        context.shadowColor = 'rgb(0, 0, 0)';
+        context.moveTo(e.clientX, e.clientY);
+      };
+      context.canvas.onmousemove = function(e) {
+        if (isDrawing) {
+          context.lineTo(e.clientX, e.clientY);
+          context.stroke();
+        }
+      };
+      context.canvas.onmouseup = function() {
+        isDrawing = false;
+      };
+
       $scope.init = function() {
-        // elements not visible at this moment, estimate size
+        // Reset on resize: elements not visible at this moment, estimate size
         $scope.width = window.innerWidth - 30; // col-xs-12 width
         $scope.height = window.innerHeight * 0.8; // row2 height
         context.canvas.width = $scope.width;
         context.canvas.height = $scope.height;
         context.clearRect(0, 0, $scope.width, $scope.height);
+
+        // Draw train background
         var train = new Image();
         train.onload = function() {
           var ratio = train.naturalHeight / train.naturalWidth;
@@ -57,8 +79,8 @@ app.directive("sprayCanvas", function($timeout) {
             width = $scope.height / ratio;
             height = $scope.height;
           }
-          context.drawImage(train, ($scope.width-width)/2, ($scope.height-height)/2, width, height);
-          $timeout(function(){
+          context.drawImage(train, ($scope.width - width) / 2, ($scope.height - height) / 2, width, height);
+          $timeout(function() {
             $scope.loaded = true;
           });
         };
