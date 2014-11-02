89b7589470466baff26d844312f2467d50234dbe
gab <gab@gablog.eu>
2014-11-02 18:28:13 +0800

    Fixed responsivness. Angular patterns using scope. Detected canvas size.

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 var app = angular.module("sprayit", []);
 
 app.controller("SprayController", function($scope, $timeout) {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  function init() {
-    $content = $("#content");
-    context.canvas.width = $content.width();
-    context.canvas.height = $content.height();
-    context.strokeStyle = 'blue';
-    context.lineWidth = '5';
-    context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
-  }
-
-  var reset = null;
-  $scope.reset = function() {
-    if (reset) {
-      clearTimeout(reset);
-    } else {
-      $timeout(function(){
-        $scope.loaded = false;
-      });
-    }
-    reset = setTimeout(function() {
-      reset = null;
-      init();
-      $timeout(function(){
-        $scope.loaded = true;
-      });
-    }, 500);
-  };
-  $(window).resize($scope.reset);
-
-  init();
-  $scope.loaded = true;
+  $timeout(function(){
+    //$scope.reset();
+  });
 });
 
+app.factory("SparyService", function() {
+  return {
+  };
+});
+
+app.directive("sprayInit", function($timeout) {
+  return {
+    scope: false,
+    restrict: 'A',
+    link: function($scope, element) {
+      var reset = null;
+      $scope.reset = function() {
+        if (reset) {
+          clearTimeout(reset);
+        } else {
+          $timeout(function() {
+            $scope.loaded = false;
+          });
+        }
+        reset = setTimeout(function() {
+          reset = null;
+          $timeout(function() {
+            $scope.init();
+          });
+        }, 500);
+      };
+      $(window).resize($scope.reset);
+    }
+  };
+});
+
+app.directive("sprayCanvas", function() {
+  return {
+    scope: false,
+    restrict: 'A',
+    link: function($scope, element) {
+      var context = element[0].getContext("2d");
+      $scope.init = function() {
+        // elements not visible at this moment, estimate size
+        $scope.width = window.innerWidth - 30; // col-xs-12 width
+        $scope.height = window.innerHeight * 0.8; // row2 height
+        context.canvas.width = $scope.width;
+        context.canvas.height = $scope.height;
+        context.strokeStyle = 'blue';
+        context.lineWidth = '5';
+        context.strokeRect(0, 0, $scope.width, $scope.height);
+        $scope.loaded = true;
+      };
+    }
+  };
+});
